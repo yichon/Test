acc2ed820a7223660c8a6608808dc9a1161f605d
Yi Chong <yi.chong.oha@gmail.com>
2017-08-30 11:07:00 +0800

    Re-Structure

---
+++ src/xyz/birdonwire/test/Test.java
@@ -0,0 +1,10 @@
+package xyz.birdonwire.test;
+
+class Test{
+
+   public static void main(String[] args){
+       System.out.println("Hello, Java!");
+       System.out.println("Next Line.");
+   }
+
+}
